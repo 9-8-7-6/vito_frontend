f6ab40d4201d0d25917371214afcae8483fcd326
9-8-7-6 <ac1246897@gmail.com>
2025-03-01 00:07:03 +0800

    init vue modify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Welcome to Vito homepage</h1>
+    <p>We provide excellent bookeeping product!</p>
   </main>
 </template>
+
+<style scoped>
+h1 {
+  color: #42b983;
+  text-align: center;
+}
+
+p {
+  text-align: center;
+  font-size: 18px;
+}
+</style>
