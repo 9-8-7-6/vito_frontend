da42356a88fe7ae622ff1986413110ae24fd82fa
9-8-7-6 <ac1246897@gmail.com>
2025-03-14 17:45:53 +0800

    add authenticated mechanism before every router action if cookie is removed or session expire, then delete user information in localstorage

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { AuthCheck } from '../api/auth.ts'
 import Home from '../views/HomeView.vue'
 import Login from '../views/LoginView.vue'
 import Register from '../views/RegisterView.vue'
+import { useAuthStore } from '@/stores/auth'
 
 const routes = [
   { path: '/', name: 'Home', component: Home, meta: { requiresAuth: true } },
@@ -16,8 +17,16 @@ const router = createRouter({
 })
 
 router.beforeEach(async (to, from, next) => {
+  const authStore = useAuthStore()
+
   const isAuthenticated = await AuthCheck()
   console.log('isAuthenticated', isAuthenticated)
+
+  if (!isAuthenticated) {
+    authStore.user = null
+    localStorage.removeItem('user')
+  }
+
   if (to.meta.requiresAuth && !isAuthenticated) {
     console.log('User not authenticated, redirecting to /login')
     next('/login')
